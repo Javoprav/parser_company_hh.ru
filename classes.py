d2606d1a3ee903e1fa1d15a966189b54b5546056
Javoprav <78508571+Javoprav@users.noreply.github.com>
2023-04-21 20:24:44 +0300

    add save_data_company_to_database

--- classes.py
+++ classes.py
@@ -1,5 +1,9 @@
 import json, requests, os
 from pprint import pprint
+from typing import List, Dict, Any
+
+from config import config
+import psycopg2
 
 
 class HH:
@@ -9,7 +13,7 @@ class HH:
         self.name = None
 
     @classmethod
-    def get_request_company(cls):
+    def get_request_company(cls) -> list[dict[str, int | None | Any]]:
         """Получает список с компаниями по api"""
         url: str = "https://api.hh.ru/vacancies"
         company_dict = []
@@ -25,7 +29,7 @@ class HH:
                     name_employer = vac['employer']['name']
                 company_dict.append({
                     'company_name': name_employer,
-                    'count-vac': data['found'],
+                    'count_vac': data['found'],
                     'company_id': id_range})
         with open('employer.json', 'w', encoding='utf8') as f:
             json.dump(company_dict, f, ensure_ascii=False)
@@ -33,7 +37,7 @@ class HH:
         return company_dict
 
     @classmethod
-    def get_request_vacancy(cls):
+    def get_request_vacancy(cls) -> list[dict[str, int | str | Any]]:
         """Получает список с вакансиями по api"""
         url: str = "https://api.hh.ru/vacancies"
         vacancy_dict = []
@@ -51,12 +55,76 @@ class HH:
                     vacancy_dict.append({
                         'company_id': id_range,
                         'company_name': vac['employer']['name'],
-                        'vacancy_name': vac['professional_roles']['name'],
-                        'salary': salary
+                        'vacancy_name': vac['name'],
+                        'salary': salary,
+                        'url': vac['alternate_url']
                     })
             else:
                 print(f"Ошибка {response.status_code}")
+        with open('vacancy.json', 'w', encoding='utf8') as f:
+            json.dump(vacancy_dict, f, ensure_ascii=False)
+            f.close()
+        return vacancy_dict
 
+    @staticmethod
+    def create_database(database_name: str, params: dict):
+        """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
 
-hhh = HH()
-pprint(hhh.get_request_vacancy())
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+        try:
+            cur.execute(f"DROP DATABASE {database_name}")
+        except psycopg2.errors.InvalidCatalogName:
+            print(f'psycopg2.errors.InvalidCatalogName: ОШИБКА:  база данных {database_name} не существует')
+
+        cur.execute(f"CREATE DATABASE {database_name}")
+        conn.close()
+        conn = psycopg2.connect(dbname=database_name, **params)
+
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE company (
+                    company_name VARCHAR(255),
+                    count_vac INTEGER,
+                    company_id INTEGER
+                )
+            """)
+
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE vacancy (
+                    company_id INTEGER,
+                    company_name VARCHAR(255),
+                    vacancy_name VARCHAR,
+                    salary INTEGER,
+                    url TEXT
+                )
+            """)
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def save_data_company_to_database(data: list[dict[str, Any]], database_name: str, params: dict):
+        """Сохранение данных о компаниях в базу данных."""
+        conn = psycopg2.connect(dbname=database_name, **params)
+        with conn.cursor() as cur:
+            for comp in data:
+                cur.execute(
+                    """
+                    INSERT INTO company (company_name, count_vac, company_id)
+                    VALUES (%s, %s, %s)
+                    """,
+                    (comp['company_name'], comp['count_vac'], comp['company_id'])
+                )
+        conn.commit()
+        conn.close()
+
+        #         cur.execute(
+        #             """
+        #             INSERT INTO vacancy (channel_id, company_name, vacancy_name, salary, url)
+        #             VALUES (%s, %s, %s, %s, %s)
+        #             """,
+        #             (channel_id, video_data['title'], video_data['publishedAt'],
+        #              f"https://www.youtube.com/watch?v={video['id']['videoId']}")
+        #         )
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from classes import *
 from pprint import pprint
-
+from config import config
 
 if __name__ == '__main__':
     hh = HH()
+    params = config()
     start_input = input('Введите "1" для запуска: ')
     if start_input.lower() == 'stop':
         print("Выход!")
         exit()
     elif start_input == '1':
-        pprint(hh.get_request_company())
+        data_company = hh.get_request_company()
+        data_vacancy = hh.get_request_vacancy()
+        hh.create_database('hh_ru', params)
+        hh.save_data_company_to_database(data_company, 'hh_ru', params)
+        # hh.save_data_vacancy_to_database(data_company, 'vacancy', params)
     else:
         print("Не верный ввод! Выход!")
         exit()
